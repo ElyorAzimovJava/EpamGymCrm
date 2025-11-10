d87da6350d1fe7687a4aa20d9b204f141f7a9197
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-11-10 11:45:50 +0500

    TrainingDao interface has been removed

--- src/main/java/com/gym/crm/dao/TrainingDao.java
+++
@@ -1,9 +0,0 @@
-package com.gym.crm.dao;
-
-import com.gym.crm.model.Training;
-
-import java.util.List;
-import java.util.UUID;
-
-public interface TrainingDao extends Dao<Training, UUID> {
-}
--- src/main/java/com/gym/crm/dao/TrainingDaoImpl.java
+++ src/main/java/com/gym/crm/dao/TrainingDaoImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public class TrainingDaoImpl implements TrainingDao {
+public class TrainingDaoImpl implements Dao<Training, UUID> {
 
     private final InMemoryStorage<Training> trainingStorage;
 
