b76616a1ed89b44fec1b0711ec8b889592b42650
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-11-23 08:25:58 +0500

    Interfaces for service have been created and impl classes moved to the right packages

---
+++ src/main/java/com/gym/crm/service/impl/TraineeServiceImpl.java
@@ -0,0 +1,59 @@
+package com.gym.crm.service.impl;
+
+import com.gym.crm.dao.TraineeDao;
+import com.gym.crm.model.Trainee;
+import com.gym.crm.service.PasswordGenerator;
+import com.gym.crm.service.TraineeService;
+import com.gym.crm.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Slf4j
+@Service
+public class TraineeServiceImpl implements TraineeService {
+
+    private TraineeDao traineeDao;
+
+    private UserService userService;
+
+    @Autowired
+    public void setTraineeDao(TraineeDao traineeDao) {
+        this.traineeDao = traineeDao;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Trainee createTrainee(Trainee trainee) {
+        String username = userService.generateUsername(trainee.getFirstName(), trainee.getLastName());
+        trainee.setUsername(username);
+        trainee.setPassword(PasswordGenerator.generatePassword(10));
+        Trainee saved = traineeDao.create(trainee);
+        log.info("Trainee with username {} saved successfully", saved.getUsername());
+        return saved;
+    }
+
+    public Trainee updateTrainee(Trainee trainee) {
+        traineeDao.update(trainee);
+        return trainee;
+    }
+
+    public void deleteTrainee(UUID id) {
+        traineeDao.delete(id);
+    }
+
+    public Trainee findById(UUID id) {
+        return traineeDao.findById(id);
+    }
+
+    public List<Trainee> listAll() {
+        return traineeDao.findAll();
+    }
+}
