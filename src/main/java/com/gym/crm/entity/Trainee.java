ff6cfaea9bb7ed8076eb76a44e347fbd380f6678
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-12-06 11:03:19 +0500

    Entity validation has been implemented

--- src/main/java/com/gym/crm/entity/Trainee.java
+++ src/main/java/com/gym/crm/entity/Trainee.java
@@ -6,6 +6,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,8 +21,10 @@ import java.util.List;
 @Table(name = "trainee")
 public class Trainee extends User {
     @Column(nullable = false)
+    @NotNull(message = "Date of birth is mandatory")
     private LocalDate dateOfBirth;
     @Column(nullable = false)
+    @NotBlank(message = "Address is mandatory")
     private String address;
     @ManyToMany(mappedBy = "trainees")
     private List<Trainer> trainers;
