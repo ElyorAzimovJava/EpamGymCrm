ba58d3d507bb6a1ef19a234e079ef480e8b0406f
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-12-10 23:10:52 +0500

    Userserive layer has been implemented (#12)

--- src/main/java/com/gym/crm/service/UserService.java
+++ src/main/java/com/gym/crm/service/UserService.java
@@ -2,4 +2,6 @@ package com.gym.crm.service;
 
 public interface UserService {
     String generateUsername(String firstName, String lastName);
+    boolean checkPassword(String username, char[] password);
+    void changePassword(String username, char[] oldPassword, char[] newPassword);
 }
